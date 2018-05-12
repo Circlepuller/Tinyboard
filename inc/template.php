4a155d7e8908a25bdb521aef5780471d94590fe4
Daniel Saunders <dsaunders@dansaunders.me>
2018-05-11 23:24:57 -0400

    Forgot that private properties are not inherited - whoops

--- inc/template.php
+++ inc/template.php
@@ -75,6 +75,9 @@ function Element($templateFile, array $options) {
  */
 class Twig_Cache_TinyboardFilesystem extends Twig_Cache_Filesystem
 {
+	private $directory;
+	private $options;
+	
 	/**
 	 * This function was removed in Twig 2.x due to developer views on the Twig library. Who says we can't keep it for ourselves though?
 	 */
