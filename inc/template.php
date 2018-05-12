71d630e122e15497196400d514e23b85383e1e5a
Daniel Saunders <dsaunders@dansaunders.me>
2018-05-11 23:32:20 -0400

    And some super calls...

--- inc/template.php
+++ inc/template.php
@@ -77,7 +77,17 @@ class Twig_Cache_TinyboardFilesystem extends Twig_Cache_Filesystem
 {
 	private $directory;
 	private $options;
-	
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __construct($directory, $options = 0)
+	{
+		parent::__construct($directory, $options);
+
+		$this->directory = $directory;
+	}
+
 	/**
 	 * This function was removed in Twig 2.x due to developer views on the Twig library. Who says we can't keep it for ourselves though?
 	 */
